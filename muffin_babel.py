7bf55787e19cae5cc829fd4a15351daf1b81d919
Kirill Klenov <horneds@gmail.com>
2016-02-15 18:28:56 +0300

    Bump version: 0.0.6 → 0.1.0

--- muffin_babel.py
+++ muffin_babel.py
@@ -15,7 +15,7 @@ from muffin.utils import slocal
 from speaklater import make_lazy_string
 
 
-__version__ = "0.0.6"
+__version__ = "0.1.0"
 __project__ = "muffin-babel"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "MIT"
