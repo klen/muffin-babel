efcd23e45b3f23da3533b6053529441ad5aae653
Kirill Klenov <horneds@gmail.com>
2021-04-21 21:57:05 +0300

    Bump version: 0.7.8 → 0.7.9

--- muffin_babel/__init__.py
+++ muffin_babel/__init__.py
@@ -16,7 +16,7 @@ from muffin.plugin import BasePlugin
 from muffin.typing import Receive, Send
 
 
-__version__ = "0.7.8"
+__version__ = "0.7.9"
 __project__ = "muffin-babel"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "MIT"
